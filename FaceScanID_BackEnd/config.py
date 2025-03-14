61c808302180e0602538d981547472bc01e6192b
Gustavo Koglin <engcomputacao.gustavokoglin@gmail.com>
2025-03-14 14:34:04 -0400

    Update config.py

--- FaceScanID_BackEnd/config.py
+++ FaceScanID_BackEnd/config.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 # Agora podemos acessar com os.getenv
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Yahweh@2027")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "***********")
 DB_NAME = os.getenv("DB_NAME", "FSID")
 DB_PORT = int(os.getenv("DB_PORT", 3306))
 
